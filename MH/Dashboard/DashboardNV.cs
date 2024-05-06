5f90279202ebd3e04c14f29c0d48ba68599d94d1
toanpham <toanphamphu2003@gmail.com>
2024-05-06 19:44:30 +0700

    add dashboards

---
+++ MH/Dashboard/DashboardNV.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTTK.MH.Dashboard
+{
+    public partial class DashboardNV : Form
+    {
+        public DashboardNV()
+        {
+            InitializeComponent();
+        }
+    }
+}
