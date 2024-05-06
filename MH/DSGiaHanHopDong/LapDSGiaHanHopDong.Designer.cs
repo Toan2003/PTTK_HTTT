7088c58ee6dd0877a13b7dfe7424049595fc1fa2
toanpham <toanphamphu2003@gmail.com>
2024-05-06 19:19:07 +0700

    solve conflict

--- MH/DSGiaHanHopDong/LapDSGiaHanHopDong.Designer.cs
+++ MH/DSGiaHanHopDong/LapDSGiaHanHopDong.Designer.cs
@@ -89,7 +89,7 @@
             this.button1.TabIndex = 5;
             this.button1.Text = "Xem danh sách";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            //this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // LapDSGiaHanHopDong
             // 
