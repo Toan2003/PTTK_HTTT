201e672ab0a961f9a45448a6c38df7f2c479de5a
The Anh <dinhtheanh753@gmail.com>
2024-05-08 11:18:54 +0700

    Fix: syntax errors

--- DBO/PhieuDangTuyenDBO.cs
+++ DBO/PhieuDangTuyenDBO.cs
@@ -1,10 +1,5 @@
 ﻿using PTTK.BUS;
 using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PTTK.DBO
 {
@@ -15,12 +10,12 @@ namespace PTTK.DBO
         {
             DataProvider = new DataProvider();
         }
-        
+
         public int ThemPhieuDangTuyen(PhieuDangTuyenBUS pdt)
         {
             string MaPDT = pdt.MaPDT;
             DateTime ThoiGianBD = pdt.ThoiGianBD;
-            string ViTriUngTuyen = pdt.ViTriUngTuyen
+            string ViTriUngTuyen = pdt.ViTriUngTuyen;
             DateTime ThoiGianKetThuc = pdt.ThoiGianKetThuc;
             int SoLuongTuyen = pdt.SoLuongTuyen;
             string ThongTinYeuCau = pdt.ThongTinYeuCau;
@@ -29,13 +24,13 @@ namespace PTTK.DBO
             string MaDN = pdt.MaDN;
             string MaDV = pdt.MaDV;
 
-            string query = $"INSERT INTO PHIEUDANGTUYEN (MAPDT, THOIGIANBD, VITRIUNGTUYEN,SOLUONGUNGTUYEN, THONGTINYEUCAU, THOIGIANKT, PHEDUYET, HINHTHUCTT,TRANGTHAITT,MADN,MADV) VALUES '{MaPDT}', '{ThoiGianBD}', {ViTriUngTuyen}, {SoLuongTuyen}, '{ThongTinYeuCau}', '{ThoiGianKetThuc}', 'Chưa phê duyệt', '{HinhThucTT}', '{TrangThaiTT}', '{MaDN}', '{MADV}' )";
+            string query = $"INSERT INTO PHIEUDANGTUYEN (MAPDT, THOIGIANBD, VITRIUNGTUYEN,SOLUONGUNGTUYEN, THONGTINYEUCAU, THOIGIANKT, PHEDUYET, HINHTHUCTT,TRANGTHAITT,MADN,MADV) VALUES '{MaPDT}', '{ThoiGianBD}', {ViTriUngTuyen}, {SoLuongTuyen}, '{ThongTinYeuCau}', '{ThoiGianKetThuc}', 'Chưa phê duyệt', '{HinhThucTT}', '{TrangThaiTT}', '{MaDN}', '{MaDV}' )";
             DataProvider.OpenConect();
             int result = DataProvider.ExecuteNonQuery(query);
             DataProvider.CloseConect();
 
             return result;
         }
-        
+
     }
 }
