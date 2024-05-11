47f37605847acb57cc418126ae0517cc4907b660
The Anh <dinhtheanh753@gmail.com>
2024-05-11 15:10:25 +0700

    Refactor: change label name of ThemDoanhNghiepMoi

--- MH/ThemDNThanhVien/ThemDoanhNghiepMoi.Designer.cs
+++ MH/ThemDNThanhVien/ThemDoanhNghiepMoi.Designer.cs
@@ -28,32 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Label lbl_DiaChi;
             this.txt_Username = new System.Windows.Forms.TextBox();
             this.txt_TenCongTy = new System.Windows.Forms.TextBox();
             this.txt_DiaChi = new System.Windows.Forms.TextBox();
             this.txt_NguoiDaiDien = new System.Windows.Forms.TextBox();
             this.txt_Email = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
+            this.lbl_Title = new System.Windows.Forms.Label();
+            this.lbl_TenCongTy = new System.Windows.Forms.Label();
+            this.lbl_MaSoThue = new System.Windows.Forms.Label();
+            this.lbl_SDT = new System.Windows.Forms.Label();
+            this.lbl_NguoiDaiDien = new System.Windows.Forms.Label();
+            this.lbl_Email = new System.Windows.Forms.Label();
             this.btn_DongY = new System.Windows.Forms.Button();
             this.btn_Huy = new System.Windows.Forms.Button();
             this.txt_SDT = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
+            this.lbl_TenDangNhap = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
             this.txt_Password = new System.Windows.Forms.TextBox();
             this.txt_ConfirmPassword = new System.Windows.Forms.TextBox();
             this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
+            this.lbl_ConfirmPassword = new System.Windows.Forms.Label();
             this.lbl_checkConfirmPwd = new System.Windows.Forms.Label();
             this.lbl_CheckEmail = new System.Windows.Forms.Label();
             this.lbl_CheckValidUsername = new System.Windows.Forms.Label();
             this.lbl_CheckValidMST = new System.Windows.Forms.Label();
             this.txt_MST = new System.Windows.Forms.TextBox();
+            lbl_DiaChi = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txt_Username
@@ -98,78 +99,78 @@
             this.txt_Email.TabIndex = 5;
             this.txt_Email.TextChanged += new System.EventHandler(this.txt_Email_TextChanged);
             // 
-            // label1
+            // lbl_Title
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(123, 37);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(348, 22);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "ĐĂNG KÝ DOANH NGHIỆP THÀNH VIÊN";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.lbl_Title.AutoSize = true;
+            this.lbl_Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Title.Location = new System.Drawing.Point(123, 37);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(348, 22);
+            this.lbl_Title.TabIndex = 6;
+            this.lbl_Title.Text = "ĐĂNG KÝ DOANH NGHIỆP THÀNH VIÊN";
+            this.lbl_Title.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // lbl_TenCongTy
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(56, 199);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 18);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Tên công ty";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.lbl_TenCongTy.AutoSize = true;
+            this.lbl_TenCongTy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_TenCongTy.Location = new System.Drawing.Point(56, 199);
+            this.lbl_TenCongTy.Name = "lbl_TenCongTy";
+            this.lbl_TenCongTy.Size = new System.Drawing.Size(85, 18);
+            this.lbl_TenCongTy.TabIndex = 7;
+            this.lbl_TenCongTy.Text = "Tên công ty";
+            this.lbl_TenCongTy.Click += new System.EventHandler(this.label2_Click);
             // 
-            // label3
+            // lbl_MaSoThue
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(56, 148);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 18);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Mã số thuế";
+            this.lbl_MaSoThue.AutoSize = true;
+            this.lbl_MaSoThue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_MaSoThue.Location = new System.Drawing.Point(56, 148);
+            this.lbl_MaSoThue.Name = "lbl_MaSoThue";
+            this.lbl_MaSoThue.Size = new System.Drawing.Size(82, 18);
+            this.lbl_MaSoThue.TabIndex = 8;
+            this.lbl_MaSoThue.Text = "Mã số thuế";
             // 
-            // label4
+            // lbl_SDT
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(56, 395);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 18);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "SDT";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
+            this.lbl_SDT.AutoSize = true;
+            this.lbl_SDT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_SDT.Location = new System.Drawing.Point(56, 395);
+            this.lbl_SDT.Name = "lbl_SDT";
+            this.lbl_SDT.Size = new System.Drawing.Size(38, 18);
+            this.lbl_SDT.TabIndex = 9;
+            this.lbl_SDT.Text = "SDT";
+            this.lbl_SDT.Click += new System.EventHandler(this.label4_Click);
             // 
-            // label5
+            // lbl_DiaChi
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(56, 250);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(53, 18);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Địa chỉ";
+            lbl_DiaChi.AutoSize = true;
+            lbl_DiaChi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lbl_DiaChi.Location = new System.Drawing.Point(56, 250);
+            lbl_DiaChi.Name = "lbl_DiaChi";
+            lbl_DiaChi.Size = new System.Drawing.Size(53, 18);
+            lbl_DiaChi.TabIndex = 10;
+            lbl_DiaChi.Text = "Địa chỉ";
             // 
-            // label6
+            // lbl_NguoiDaiDien
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(56, 297);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(101, 18);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "Người đại diện";
+            this.lbl_NguoiDaiDien.AutoSize = true;
+            this.lbl_NguoiDaiDien.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_NguoiDaiDien.Location = new System.Drawing.Point(56, 297);
+            this.lbl_NguoiDaiDien.Name = "lbl_NguoiDaiDien";
+            this.lbl_NguoiDaiDien.Size = new System.Drawing.Size(101, 18);
+            this.lbl_NguoiDaiDien.TabIndex = 11;
+            this.lbl_NguoiDaiDien.Text = "Người đại diện";
             // 
-            // label7
+            // lbl_Email
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(56, 345);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(91, 18);
-            this.label7.TabIndex = 12;
-            this.label7.Text = "Email liên hệ";
+            this.lbl_Email.AutoSize = true;
+            this.lbl_Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Email.Location = new System.Drawing.Point(56, 345);
+            this.lbl_Email.Name = "lbl_Email";
+            this.lbl_Email.Size = new System.Drawing.Size(91, 18);
+            this.lbl_Email.TabIndex = 12;
+            this.lbl_Email.Text = "Email liên hệ";
             // 
             // btn_DongY
             // 
@@ -204,26 +205,26 @@
             this.txt_SDT.Size = new System.Drawing.Size(305, 22);
             this.txt_SDT.TabIndex = 6;
             // 
-            // label8
+            // lbl_TenDangNhap
             // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(56, 102);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(131, 23);
-            this.label8.TabIndex = 16;
-            this.label8.Text = "Tên đăng nhập";
+            this.lbl_TenDangNhap.AutoSize = true;
+            this.lbl_TenDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_TenDangNhap.Location = new System.Drawing.Point(56, 102);
+            this.lbl_TenDangNhap.Name = "lbl_TenDangNhap";
+            this.lbl_TenDangNhap.Size = new System.Drawing.Size(105, 18);
+            this.lbl_TenDangNhap.TabIndex = 16;
+            this.lbl_TenDangNhap.Text = "Tên đăng nhập";
             // 
-            // label9
+            // lbl_Password
             // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(56, 447);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(69, 18);
-            this.label9.TabIndex = 17;
-            this.label9.Text = "Mật khẩu";
-            this.label9.Click += new System.EventHandler(this.label9_Click);
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Password.Location = new System.Drawing.Point(56, 447);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(69, 18);
+            this.lbl_Password.TabIndex = 17;
+            this.lbl_Password.Text = "Mật khẩu";
+            this.lbl_Password.Click += new System.EventHandler(this.label9_Click);
             // 
             // txt_Password
             // 
@@ -252,15 +253,15 @@
             this.label10.Size = new System.Drawing.Size(0, 16);
             this.label10.TabIndex = 20;
             // 
-            // label11
+            // lbl_ConfirmPassword
             // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(56, 495);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(126, 18);
-            this.label11.TabIndex = 21;
-            this.label11.Text = "Nhập lại mật khẩu";
+            this.lbl_ConfirmPassword.AutoSize = true;
+            this.lbl_ConfirmPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ConfirmPassword.Location = new System.Drawing.Point(56, 495);
+            this.lbl_ConfirmPassword.Name = "lbl_ConfirmPassword";
+            this.lbl_ConfirmPassword.Size = new System.Drawing.Size(126, 18);
+            this.lbl_ConfirmPassword.TabIndex = 21;
+            this.lbl_ConfirmPassword.Text = "Nhập lại mật khẩu";
             // 
             // lbl_checkConfirmPwd
             // 
@@ -313,22 +314,22 @@
             this.Controls.Add(this.lbl_CheckValidUsername);
             this.Controls.Add(this.lbl_CheckEmail);
             this.Controls.Add(this.lbl_checkConfirmPwd);
-            this.Controls.Add(this.label11);
+            this.Controls.Add(this.lbl_ConfirmPassword);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.txt_ConfirmPassword);
             this.Controls.Add(this.txt_Password);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_TenDangNhap);
             this.Controls.Add(this.txt_SDT);
             this.Controls.Add(this.btn_Huy);
             this.Controls.Add(this.btn_DongY);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_Email);
+            this.Controls.Add(this.lbl_NguoiDaiDien);
+            this.Controls.Add(lbl_DiaChi);
+            this.Controls.Add(this.lbl_SDT);
+            this.Controls.Add(this.lbl_MaSoThue);
+            this.Controls.Add(this.lbl_TenCongTy);
+            this.Controls.Add(this.lbl_Title);
             this.Controls.Add(this.txt_Email);
             this.Controls.Add(this.txt_NguoiDaiDien);
             this.Controls.Add(this.txt_DiaChi);
@@ -351,22 +352,21 @@
         private System.Windows.Forms.TextBox txt_DiaChi;
         private System.Windows.Forms.TextBox txt_NguoiDaiDien;
         private System.Windows.Forms.TextBox txt_Email;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbl_Title;
+        private System.Windows.Forms.Label lbl_TenCongTy;
+        private System.Windows.Forms.Label lbl_MaSoThue;
+        private System.Windows.Forms.Label lbl_SDT;
+        private System.Windows.Forms.Label lbl_NguoiDaiDien;
+        private System.Windows.Forms.Label lbl_Email;
         private System.Windows.Forms.Button btn_DongY;
         private System.Windows.Forms.Button btn_Huy;
         private System.Windows.Forms.TextBox txt_SDT;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lbl_TenDangNhap;
+        private System.Windows.Forms.Label lbl_Password;
         private System.Windows.Forms.TextBox txt_Password;
         private System.Windows.Forms.TextBox txt_ConfirmPassword;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lbl_ConfirmPassword;
         private System.Windows.Forms.Label lbl_checkConfirmPwd;
         private System.Windows.Forms.Label lbl_CheckEmail;
         private System.Windows.Forms.Label lbl_CheckValidUsername;
